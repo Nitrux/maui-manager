e0eba9b4a709a9abd11528a6a3d34204937c4341
Uri Herrera <hello@uriherrera.com>
2022-07-15 20:45:45 -0500

    ...

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# mauiman
-Maui Manager
+# Maui Manager
+![](https://mauiman.org/wp-content/uploads/2018/12/maui_project_logo.png)
+
+[![License: LGPL v3](https://img.shields.io/badge/License-LGPL%20v3-blue.svg)](https://www.gnu.org/licenses/lgpl-3.0) [![Awesome](https://awesome.re/badge.svg)](https://awesome.re) [![Generic badge](https://img.shields.io/badge/OS-Linux-blue.svg)](https://shields.io/)
+
+_Maui Manager Library. Server and public library API._
+
+# Build
+
+### Requirements
+
+#### Debian/Ubuntu
+
+```
+qtbase5-dev
+```
+
+### Compile source
+ 1. `git clone --depth 1 --branch v2.1 https://invent.kde.org/maui/mauiman.git` 
+ 2. `mkdir -p mauiman/build && cd mauiman/build`
+ 3. `cmake -DCMAKE_INSTALL_PREFIX=/usr -DENABLE_BSYMBOLICFUNCTIONS=OFF -DQUICK_COMPILER=ON -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_SYSCONFDIR=/etc -DCMAKE_INSTALL_LOCALSTATEDIR=/var -DCMAKE_EXPORT_NO_PACKAGE_REGISTRY=ON -DCMAKE_FIND_PACKAGE_NO_PACKAGE_REGISTRY=ON -DCMAKE_INSTALL_RUNSTATEDIR=/run "-GUnix Makefiles" -DCMAKE_VERBOSE_MAKEFILE=ON -DCMAKE_INSTALL_LIBDIR=lib/x86_64-linux-gnu ..`
+ 4. `make`
+
+ ### Install
+ 1. `make install`
+
+# Issues
+If you find problems with the contents of this repository please create an issue.
+
+©2022 Nitrux Latinoamericana S.C.
---
+++ gh-build.sh
@@ -0,0 +1,93 @@
+#!/bin/bash
+
+set -x
+
+### Install Build Tools #1
+
+DEBIAN_FRONTEND=noninteractive apt -qq update
+DEBIAN_FRONTEND=noninteractive apt -qq -yy install --no-install-recommends \
+	appstream \
+	automake \
+	autotools-dev \
+	build-essential \
+	checkinstall \
+	cmake \
+	curl \
+	devscripts \
+	equivs \
+	extra-cmake-modules \
+	gettext \
+	git \
+	gnupg2 \
+	lintian \
+	wget
+
+### Add Neon Sources
+
+wget -qO /etc/apt/sources.list.d/neon-user-repo.list https://raw.githubusercontent.com/Nitrux/iso-tool/development/configs/files/sources.list.neon.user
+
+DEBIAN_FRONTEND=noninteractive apt-key adv --keyserver keyserver.ubuntu.com --recv-keys \
+	55751E5D > /dev/null
+
+DEBIAN_FRONTEND=noninteractive apt -qq update
+
+### Install Package Build Dependencies #1
+
+DEBIAN_FRONTEND=noninteractive apt -qq -yy install --no-install-recommends \
+	qtbase5-dev
+
+### Clone Repository
+
+git clone --depth 1 --branch master https://invent.kde.org/maui/mauiman.git
+
+rm -rf mauiman/{README.md}
+
+### Compile Source
+
+mkdir -p mauiman/build && cd mauiman/build
+
+cmake \
+	-DCMAKE_INSTALL_PREFIX=/usr \
+	-DENABLE_BSYMBOLICFUNCTIONS=OFF \
+	-DQUICK_COMPILER=ON \
+	-DCMAKE_BUILD_TYPE=Release \
+	-DCMAKE_INSTALL_SYSCONFDIR=/etc \
+	-DCMAKE_INSTALL_LOCALSTATEDIR=/var \
+	-DCMAKE_EXPORT_NO_PACKAGE_REGISTRY=ON \
+	-DCMAKE_FIND_PACKAGE_NO_PACKAGE_REGISTRY=ON \
+	-DCMAKE_INSTALL_RUNSTATEDIR=/run "-GUnix Makefiles" \
+	-DCMAKE_VERBOSE_MAKEFILE=ON \
+	-DCMAKE_INSTALL_LIBDIR=lib/x86_64-linux-gnu ..
+
+make
+
+### Run checkinstall and Build Debian Package
+### DO NOT USE debuild, screw it
+
+>> description-pak printf "%s\n" \
+	'A free and modular front-end framework for developing user experiences.' \
+	'' \
+	'This package contains the Maui Manager shared library, the server and' \
+	'the public API.' \
+	'' \
+	''
+
+checkinstall -D -y \
+	--install=no \
+	--fstrans=yes \
+	--pkgname=mauiman-git \
+	--pkgversion=2.2.0+git+1 \
+	--pkgarch=amd64 \
+	--pkgrelease="1" \
+	--pkglicense=LGPL-3 \
+	--pkggroup=libs \
+	--pkgsource=mauiman \
+	--pakdir=../.. \
+	--maintainer=uri_herrera@nxos.org \
+	--provides=mauiman-git \
+	--requires=libc6,libqt5core5a \
+	--nodoc \
+	--strip=no \
+	--stripso=yes \
+	--reset-uids=yes \
+	--deldesc=yes
